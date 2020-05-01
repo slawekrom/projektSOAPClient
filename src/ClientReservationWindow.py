72da5c0db6f00dd732447ac637b0f27142f2aea6
kubson3824 <fizyk72@gmail.com>
2020-05-01 21:33:14 +0200

    Klient w większej mierze działający

--- src/ClientReservationWindow.py
+++ src/ClientReservationWindow.py
@@ -12,17 +12,29 @@ class ClientReservationWindow(BaseWidget):
         BaseWidget.__init__(self, 'Person window')
         # Definition of the forms fields
         self._client = client
-        self._dateField = ControlText('Date', enabled=False, default=screening_info['date'])
-        self._timeField = ControlText('Time', enabled=False, default=screening_info['time'])
-        self._titleField = ControlText('Title', enabled=False, default=screening_info['title'])
+        self._dateField = ControlText('Date', enabled=False, default=self._screening_info['date'].strftime("%d-%m-%Y"))
+        self.__timeField = ControlText('Time', enabled=False, default=self._screening_info['date'].strftime("%H:%M"))
+        self._titleField = ControlText('Title', enabled=False, default=self._screening_info['movie']['title'])
         self._seatsField = ControlTextArea('Seats', enabled=False)
         self._paidField = ControlCheckBox('Is paid?')
-        self._buttonField = ControlButton('Pay')
+        self._payButton = ControlButton('Pay')
+        self._editReservationButton = ControlButton('Edit')
+        self._removeReservationButton = ControlButton('Remove')
 
         # Define the button action
-        self._buttonField.value = self.__buttonAction
+        self._payButton.value = self._payAction
+        self._editReservationButton.value = self._editAction
+        self._removeReservationButton.value = self._removeAction
 
-    def __buttonAction(self):
+        self.formset = ['_titleField', '_buttonField']
+
+    def _payAction(self):
+        self._client.service.ifPlacesFree(self)
+
+    def _editAction(self):
+        self._client.service.ifPlacesFree(self)
+
+    def _removeAction(self):
         self._client.service.ifPlacesFree(self)
 
     # Execute the application
